0a2f2e5cb5c4b1739b399e659a94634830749a9e
Omiganox <omiganox@Orionox.home>
2019-03-08 14:09:18 +0100

    MAJ de la documentation

--- CreateBDD.rb
+++ CreateBDD.rb
@@ -15,16 +15,37 @@ require_relative "connectSqlite3.rb"
 # des grilles qu'il a complété et des scores qu'il a obtenu.
 #
 ActiveRecord::Schema.define do
+	##
+	#
+	# == Table *Profil*
+	#
+	# Contient l'identifiant d'un compte joueur, son pseudonyme et son mot de passe encrypté (impossible à voir
+	# en clair dans la BDD).
+	#
 	create_table :profils do |c|
 		c.string :pseudonyme
 		c.string :mdpEncrypted
 	end
 
+	##
+	#
+	# == Table *Map*
+	#
+	# Contient l'identifiant d'une grille de jeu, sa taille (nombre de cases en longueur/largeur) et sa
+	# difficulté.
+	#
   create_table :maps do |c|
     c.string :taille
     c.string :difficulte
   end
 
+	##
+	#
+	# == Table *Score*
+	#
+	# Contient l'identifiant d'un score, sa valeur numérique, le mode de jeu dans lequel il a été obtenu,
+	# sa date d'obtention, plus les identifiants de la grille concernée et du joueur propriétaire.
+	#
   create_table :scores do |c|
   	c.integer :montantScore
   	c.string :modeJeu
