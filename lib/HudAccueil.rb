876b8b6dc40fad3244f468c80f0034601859a619
Percedal <moi.durand.72@gmail.com>
2019-03-08 22:28:57 +0100

    facto hud acceuil

--- lib/HudAccueil.rb
+++ lib/HudAccueil.rb
@@ -1,57 +1,44 @@
 class HudAccueil < Hud
+	# @btnConnecter
+	# @btnInscrire
+	# @btnQuitter
+	# @entryIdentifiant
+	# @entryMotDePasse
+
 	def initialize(window)
 		super(window)
-
-		#Boutons : S'inscrire et Se connecter
-		@btnInscrire = Gtk::Button.new
-		@btnInscrire.set_label("S'inscrire")
-		@btnConnecter = Gtk::Button.new
-		@btnConnecter.set_label("Se connecter")
-
-		#Label : Identifiant et mot de passe
-		@lblId = Gtk::Label.new("Identifiant : ")
-		@lbMdp = Gtk::Label.new("Mot de passe : ")
-
-		self.attach(@lblId,8,6,2,2)
-		self.attach(@lbMdp,8,8,2,2)
-
-		#Mise des boutons dans la grille
-		self.attach(@btnInscrire,8,10,2,2)
-		self.attach(@btnConnecter,10,10,2,2)
-
-		#Entrée de texte : Identifiant et Mot de passe
-		@entIdentifiant = Gtk::Entry.new
-		@entMotDePasse = Gtk::Entry.new
-
-		#Mise des entrées dans la grille
-		self.attach(@entIdentifiant,10,6,2,2)
-		self.attach(@entMotDePasse,10,8,2,2)
-
-		#Bouton option
-		# @btnOption = Gtk::Button.new :label => "Options"
-		@btnQuitter = Gtk::Button.new :label => "Quitter"
-
-		# self.attach(@btnOption,4,14,4,2)
-		self.attach(@btnQuitter,14,14,4,2)
+		@entryIdentifiant = Gtk::Entry.new
+		@entryMotDePasse = Gtk::Entry.new
 
 		self.initBoutonConnecter
+		self.initBoutonInscription
 		self.initBoutonQuitter
-		# self.initBoutonOptions
+
+		self.attach(@btnConnecter,10,10,2,2)
+		self.attach(@btnInscrire,8,10,2,2)
+		self.attach(@btnQuitter,14,14,4,2)
+		self.attach(@entryIdentifiant,10,6,2,2)
+		self.attach(@entryMotDePasse,10,8,2,2)
+		self.attach(Gtk::Label.new("Identifiant : "),8,6,2,2)
+		self.attach(Gtk::Label.new("Mot de passe : "),8,8,2,2)
 	end
 
 
 	def initBoutonConnecter
+		@btnConnecter = Gtk::Button.new :label => "Se connecter"
 		@btnConnecter.signal_connect("clicked") {
-				@fenetre.changerWidget(self,HudModeDeJeu.new(@fenetre))
+				self.lancementModeJeu
 		}
 	end
 
-	def initBoutonQuitter
+	def initBoutonInscription
+		puts "Inscription => Traitement manquant"
+		@btnInscrire = Gtk::Button.new :label => "S'inscrire"
+	end
 
-		@btnQuitter.signal_connect('clicked') {
-			puts "Fermeture de l'application !"
-			Gtk.main_quit
-		}
+	def initBoutonQuitter
+		@btnQuitter = Gtk::Button.new :label => "Quitter"
+		@btnQuitter.signal_connect('clicked') {	Gtk.main_quit }
 	end
 
 end
