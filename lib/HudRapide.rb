b26c8ca5d31e1e7b54b39255c19ab75781c22e7c
Meiiah <marion.philippe72@gmail.com>
2019-03-11 15:01:44 +0100

    Ajout du timer

--- lib/HudRapide.rb
+++ lib/HudRapide.rb
@@ -5,24 +5,46 @@ class HudRapide < HudJeu
 	def initialize(window,grille)
 		super(window,grille)
 		@btnPause = Gtk::Button.new :label => "Pause"
-
+		@lblTime = Gtk::Label.new(" 0:0 ")
+		@timer = Time.now
+		@pause = false
+		@horloge = 0
+		@stockHorloge = 0
 		self.setTitre("Partie rapide")
 		self.setDesc("Ici la desc du mode rapide")
 
 
 		self.initBoutonOptions
 		initBoutonPause
+		
 
 		self.attach(@btnPause,5,0,2,1)
+		self.attach(@lblTime,0,2,1,1)
+
+		@t=Thread.new{timer}
+	end
+
+	def timer
+		while true do
+			@horloge = (Time.now - @timer) + @stockHorloge
+			@lblTime.set_label((@horloge/60).to_i.to_s + ":" + (@horloge%60).to_i.to_s)
+			sleep 1
+		end
 	end
 
 	def initBoutonPause
 		@btnPause.signal_connect('clicked'){
-			@timer.stop
-			@btnPause.set_label("Play")
-				@btnPause.signal_connect('clicked'){
-					@timer.continue
-				}
+			if @pause
+				@timer = Time.now
+				@t = Thread.new{timer}
+				@btnPause.set_label("Pause")
+				@pause = false
+			else
+				@stockHorloge = @stockHorloge + (Time.now - @timer)
+				@t.kill
+				@btnPause.set_label("Play")
+				@pause = true
+			end
 		}
 
 	end
