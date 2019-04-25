cd17cb17ed0374717c3de6d6af9ac7a9e9a6949b
Clément <cl.magniez@laposte.net>
2019-04-25 17:34:59 +0200

    Ajout d'un script d'installation

--- install.sh
+++ install.sh
@@ -1,41 +1,47 @@
-!/usr/bin/env bash
+#!/usr/bin/env bash
 
-OS=`lsb_release -ds 2>/dev/null || cat /etc/*release 2>/dev/null | head -n1 || uname -om`
+#OS=`lsb_release -ds 2>/dev/null || cat /etc/*release 2>/dev/null | head -n1 || uname -om`
 
-OS=$(echo $OS | cut -d ' ' -f1)
+#OS=$(echo $OS | cut -d ' ' -f1)
 
-if [ $OS = "Ubuntu" ] || [ $OS = "Debian"]; then
-	sudo apt-get install ruby > /dev/null
-	echo "Installation de Ruby"
-	echo "Installation du gestionnaire de gemmes Ruby"
-	sudo apt-get install rubygems > /dev/null
-	echo "Installation des gemmes nécessaires (cette opération peut prendre un certain temps)"
-	echo "Installation de ActiveRecord"
-	sudo gem install activerecord > /dev/null
-	echo "Installation de IniFile"
-	sudo gem install inifile > /dev/null
-	echo "Installation de GTK"
-	sudo gem install gtk3 > /dev/null
-fi
+#if [ $OS = "Ubuntu" ] || [ $OS = "Debian"]; then
+#	sudo apt-get install ruby-full > /dev/null
+#	echo "Installation de Ruby"
+#	echo "Installation de SQLite"
+# sudo apt-get install libsqlite3-dev > /dev/null
+#	echo "Installation de GTK3"
+# sudo apt-get install ruby-gtk3 > /dev/null
+#	echo "Installation du gestionnaire de gemmes Ruby"
+#	sudo apt-get install rubygems > /dev/null
+#	echo "Installation des gemmes nécessaires (cette opération peut prendre un certain temps)"
+#	echo "Installation de la gemme SQLite"
+#	sudo gem install sqlite3 > /dev/null
+#	echo "Installation de la gemme ActiveRecord"
+#	sudo gem install activerecord > /dev/null
+#	echo "Installation de la gemme IniFile"
+#	sudo gem install inifile > /dev/null
+#	echo "Installation de la gemme GTK"
+#	sudo gem install gtk3 > /dev/null
+#fi
+
 
 path=`pwd`
 
-echo $path
+#echo -e "!/usr/bin/env bash\nruby ${path}/lib/main.rb" > tents-trees
+#chmod +x tents-trees
+#echo -e "export PATH=$PATH:${path}" >> ~/.profile 
+#source ~/.profile
 
-#desk_path="/usr/share/applications/tents.desktop"
-#if [ -e $desk_path ]; then
-#	echo "Impossible de créer un raccourci bureau : un fichier sous ce nom existe déjà."
-#else
-#	sudo sh -c "echo '[Desktop Entry]
-#Version=1.0 
-#Type=Application
-#Terminal=false
-#Exec=${path}/bin/main.rb
-#Name=Tents And Trees
-#Comment=L3S5 info
-#Icon=${path}/img/tents.png
-#Categories=Game;' >> $desk_path"
-#	echo "Fichier de lancement 'Tents And Trees' créé."
-#fi
-
+echo -e "[Desktop Entry]
+Version=1.0 
+Type=Application
+Terminal=false
+Icon=${path}/img/icon.png
+Exec=${path}/tents-trees
+Name=Tents And Trees
+Comment=L3S5 info
+Categories=Game;" > tents.desktop
+echo "Fichier de lancement 'Tents And Trees' créé."
+chmod +x tents.desktop
+sudo cp tents.desktop /usr/share/applications/tents.desktop &> /dev/null
 
---
+++ readme.md
@@ -0,0 +1,30 @@
+Projet "jeu de tentes et arbre" de fin de L3.
+
+# Comment jouer
+
+## Installation
+Après avoir téléchargé le zip ou cloné le repo, exécuter `./install.sh` si sur une distro Debian/Ubuntu ; sur une autre, installer à la main GTK et Ruby ainsi que les gemmes gtk3, inifile et activerecord.
+
+## Exécution
+Entrer `./tents-trees` depuis la racine du projet.
+
+### Jouer
+
+Commencer par créer un compte en cliquant sur "S'inscrire" depuis l'écran d'accueil, puis se connecter via ces identifiants. Choisir un mode de difficulté puis (si non-tutoriel) un niveau de difficulté ; les règles sont systématiquement disponibles via un bouton en haut à droite de la fenêtre.
+La partie s'achèvera automatiquement quand la grille sera complète et valide, présentant un écran résumant le score et le temps mis à la compléter.
+Une configuration basique est proposée dans le menu d'options, accessible en bas à gauche ; il est possible de renommer son compte / changer son mot de passe et de voir ses scores dans le profil.
+
+Quatre modes de jeu sont proposés :
+- Tutoriel
+- Chrono (la partie s'achevant, et le score étant ignoré, si le décompte atteint 00:00)
+- Aventure (cinq parties de suite sur une même taille de grille avant de passer à la suivante, la taille de départ dépendant de la difficulté)
+- Exploration (mode libre)
+
+# Membres
+- BUON Romane
+- DURAND Pierre
+- KAJAK Rémi 
+- LALOY Alexandre
+- LOUP Renaud
+- MAGNIEZ Clément
+- PHILIPPE Marion
