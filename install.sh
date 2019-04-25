71bcced5a782066909bbd92c6a66107327379c49
Clément <cl.magniez@laposte.net>
2019-04-25 18:19:45 +0200

    ajout de ruby-sqlite à install.sh

--- install.sh
+++ install.sh
@@ -1,28 +1,29 @@
 #!/usr/bin/env bash
 
-OS=`lsb_release -ds 2>/dev/null || cat /etc/*release 2>/dev/null | head -n1 || uname -om`
+#OS=`lsb_release -ds 2>/dev/null || cat /etc/*release 2>/dev/null | head -n1 || uname -om`
 
-OS=$(echo $OS | cut -d ' ' -f1)
+#OS=$(echo $OS | cut -d ' ' -f1)
 
-if [ $OS = "Ubuntu" ] || [ $OS = "Debian"]; then
-	sudo apt-get install ruby-full > /dev/null
+#if [ $OS = "Ubuntu" ] || [ $OS = "Debian"]; then
+		sudo apt-get install ruby-full > /dev/null
 	echo "Installation de Ruby"
 	echo "Installation de SQLite"
- sudo apt-get install libsqlite3-dev > /dev/null
+		sudo apt-get install libsqlite3-dev > /dev/null
+		sudo apt-get install ruby-sqlite3 > /dev/null
 	echo "Installation de GTK3"
- sudo apt-get install ruby-gtk3 > /dev/null
+		sudo apt-get install ruby-gtk3 > /dev/null
 	echo "Installation du gestionnaire de gemmes Ruby"
-	sudo apt-get install rubygems > /dev/null
+		sudo apt-get install rubygems > /dev/null
 	echo "Installation des gemmes nécessaires (cette opération peut prendre un certain temps)"
-	echo "Installation de la gemme SQLite"
-	sudo gem install sqlite3 > /dev/null
-	echo "Installation de la gemme ActiveRecord"
-	sudo gem install active_record > /dev/null
-	echo "Installation de la gemme IniFile"
-	sudo gem install inifile > /dev/null
-	echo "Installation de la gemme GTK"
-	sudo gem install gtk3 > /dev/null
-fi
+		echo "Installation de la gemme SQLite"
+			sudo gem install sqlite3 > /dev/null
+		echo "Installation de la gemme ActiveRecord"
+			sudo gem install active_record > /dev/null
+		echo "Installation de la gemme IniFile"
+			sudo gem install inifile > /dev/null
+		echo "Installation de la gemme GTK"
+			sudo gem install gtk3 > /dev/null
+#fi
 
 
 path=`pwd`
