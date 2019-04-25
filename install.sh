373ac99b3ba6397065f035ca921be501d35caddb
Clément <cl.magniez@laposte.net>
2019-04-25 18:21:04 +0200

    Fix de la gemme activerecord erronée dans install

--- install.sh
+++ install.sh
@@ -18,7 +18,7 @@
 		echo "Installation de la gemme SQLite"
 			sudo gem install sqlite3 > /dev/null
 		echo "Installation de la gemme ActiveRecord"
-			sudo gem install active_record > /dev/null
+			sudo gem install activerecord > /dev/null
 		echo "Installation de la gemme IniFile"
 			sudo gem install inifile > /dev/null
 		echo "Installation de la gemme GTK"
